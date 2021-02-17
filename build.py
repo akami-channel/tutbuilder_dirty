8e66c9612431235b796403de3f32c977e61e9eb4
akami <akami.channel@gmail.com>
2021-02-17 13:33:03 +0900

    sssthrough pythontart "Sulbime Text" "C:\Program Files\Sublime Text 3\sublime_text.exe"

--- build.py
+++ build.py
@@ -5,7 +5,17 @@ import re
 import hashlib
 
 
-with open("01_plain_svg.svg", 'r', encoding='utf-8') as file:
-	contents = file.read()
+# with open("01_plain_svg.svg", 'r', encoding='utf-8') as file:
+# 	contents = file.read()
 
-lined_contents = contents.split("\n")
+# lined_contents = contents.split("\n")
+
+
+name1 = "test_file1"
+name2 = "test_file2"
+fin = open(name1, "r")
+data2 = fin.read()
+fin.close()
+fout = open(name2, "a")
+fout.write(data2)
+fout.close()
